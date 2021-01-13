c17314c2d391af38d3e95ef19239136995f38322
ksmxxxxxx <kankichi69@gmail.com>
2021-01-13 18:29:48 +0900

    :pencil2: Fix use prepare statement to SQL

--- note.rb
+++ note.rb
@@ -3,30 +3,38 @@ require 'securerandom'
 require 'pg'
 
 class Note
+  @connection = PG.connect( dbname: 'note_app' )
+  order_by_id_desc = "SELECT * FROM note ORDER BY id DESC"
+  get_by_id = "SELECT * FROM note WHERE id=$1"
+  create = "INSERT INTO note(title, body) VALUES ($1, $2)"
+  edit = "UPDATE note SET title=$1, body=$2 WHERE id=$3"
+  delete = "DELETE FROM note WHERE id=$1"
+
+  @connection.prepare('render', order_by_id_desc)
+  @connection.prepare('get_by_id', get_by_id)
+  @connection.prepare('create', create)
+  @connection.prepare('edit', edit)
+  @connection.prepare('delete', delete)
+
   class << self
     def render
-      connection = PG.connect( dbname: 'note_app' )
-      connection.exec( "SELECT * FROM note ORDER BY id DESC" )
+      @connection.exec_prepared( 'render' )
     end
 
     def get_by_id(id: note_id)
-      connection = PG.connect( dbname: 'note_app' )
-      connection.exec( "SELECT * FROM note WHERE id=#{id}" )
+      @connection.exec_prepared( 'get_by_id', [id] )
     end
 
     def create(title: note_title, body: note_body)
-      connection = PG.connect( dbname: 'note_app' )
-      connection.exec( "INSERT INTO note(title, body) VALUES ('#{title}', '#{body}')" )
+      @connection.exec_prepared( 'create', [title, body] )
     end
 
     def edit(id: note_id, title: note_title, body: note_body)
-      connection = PG.connect( dbname: 'note_app' )
-      connection.exec( "UPDATE note SET title='#{title}', body='#{body}' WHERE id=#{id}" )
+      @connection.exec_prepared( 'edit', [title, body, id] )
     end
 
     def delete(id: note_uuid)
-      connection = PG.connect( dbname: 'note_app' )
-      connection.exec( "DELETE FROM note WHERE id=#{id}" )
+      @connection.exec_prepared( 'delete', [id] )
     end
   end
 end
